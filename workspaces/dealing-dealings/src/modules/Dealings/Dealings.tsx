0f46be5b810fc2597a8950f9a01a26e659079bf8
Richie Thomas <ricthomas@linz.govt.nz>
2023-09-14 14:48:25 +1200

    feat: Add link to dealings, working

--- workspaces/dealing-dealings/src/modules/Dealings/Dealings.tsx
+++ workspaces/dealing-dealings/src/modules/Dealings/Dealings.tsx
@@ -1,5 +1,6 @@
 import {IDealing} from "./models/Dealing.ts";
 import "./Dealings.scss";
+import {Link} from "react-router-dom";
 
 export const Dealings = (props: {
     dealings: IDealing[];
@@ -8,11 +9,11 @@ export const Dealings = (props: {
         <section className={"Dealings"}>
             <h1>Dealings</h1>
             {props.dealings.map((dealing: IDealing) => (
-                <div className={"Dealings-dealing"}>
+                <Link className={"Dealings-dealing"} to={`/dealings/${dealing.dealingId}`}>
                     <span>{dealing.clientRef1}</span>
                     <span>{dealing.clientRef2}</span>
                     <span>{dealing.dealingId}</span>
-                </div>
+                </Link>
             ))}
         </section>
     )
